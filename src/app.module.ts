43904ec10baf3458cb6d2f591ed3cdbf2e26776f
DoroshenkoMaxim <36296940+DoroshenkoMaxim@users.noreply.github.com>
2025-01-08 04:01:27 +0300

    kjhjk

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,6 @@ import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
 @Module({
   imports: [
     // Добавляем модуль для раздачи статики
@@ -15,7 +12,5 @@ import { AppService } from './app.service';
       // exclude: ['/api*'],
     }),
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
-}
-bootstrap();
+@Module({
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'), 
+      serveRoot: './',
+    }),
+  ],
+})
+export class AppModule {}
