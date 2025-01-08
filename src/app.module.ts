35bd851e27d6c1202719f460e84b8732fbd1dd6d
DoroshenkoMaxim <36296940+DoroshenkoMaxim@users.noreply.github.com>
2025-01-08 04:21:18 +0300

    fgh

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,10 @@ import { join } from 'path';
   imports: [
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
+      serveRoot: '/',
     }),
   ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule { }
