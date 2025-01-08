b4bc2529b175eec848f41b0dfb06bd6de9d96044
DoroshenkoMaxim <36296940+DoroshenkoMaxim@users.noreply.github.com>
2025-01-08 04:05:23 +0300

    dfg

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,7 @@ import { join } from 'path';
 @Module({
   imports: [
     ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', 'public'), 
-      serveRoot: '/',
+      rootPath: join(__dirname, '..', 'public'),
     }),
   ],
 })
